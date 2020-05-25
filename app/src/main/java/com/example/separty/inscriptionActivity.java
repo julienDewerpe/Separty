de001693713dd1479a26fdf18b9979a778f23bca
Loadeksdi <nicolas.load@gmail.com>
2020-05-25 17:36:03 +0200

    we can change from activity

--- app/src/main/java/com/example/separty/inscriptionActivity.java
+++ app/src/main/java/com/example/separty/inscriptionActivity.java
@@ -1,7 +1,16 @@
 package com.example.separty;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
 /**
  * created by separty team on <Date du jour>
  **/
-public class inscriptionActivity {
+public class inscriptionActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+    }
 }
