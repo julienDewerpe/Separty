c9948ac94b48211bb676020d76e663a54ce515f4
julien dewerpe <julien.dewerpe@orange.fr>
2020-05-28 10:38:25 +0200

    vue des questions

---
+++ app/src/main/java/com/example/separty/activity_question.java
@@ -0,0 +1,14 @@
+package com.example.separty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
