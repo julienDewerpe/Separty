c28d971ebb9b3152128cb89b2fc9acde2c867790
julien dewerpe <julien.dewerpe@orange.fr>
2020-05-28 09:42:57 +0200

    test unfocused

--- app/src/main/java/com/example/separty/Login.java
+++ app/src/main/java/com/example/separty/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,10 +16,13 @@ public class Login extends AppCompatActivity {
     private Button signUp;
     private Button login;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         signUp = findViewById(R.id.signUp);
         signUp.setOnClickListener(new View.OnClickListener() {
